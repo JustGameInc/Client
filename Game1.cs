2a5e30db991b5f61a5e2905153da963c7a0fe931
voidptr_t <39582793+Sanceilaks@users.noreply.github.com>
2023-01-21 21:00:56 +0300

    Initial

---
+++ Game1.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MyGame
+{
+    public class Game1 : Game
+    {
+		private Texture2D _zatupTexture;
+
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+			_zatupTexture = Content.Load<Texture2D>("Textures/Player");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+			_spriteBatch.Begin();
+			_spriteBatch.Draw(_zatupTexture, Vector2.Zero, Color.White);
+			_spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
