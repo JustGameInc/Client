59cb1e32a897af861eceb593cd61a2d6832d75cf
voidptr_t <39582793+Sanceilaks@users.noreply.github.com>
2023-01-22 00:26:08 +0300

    Entity

--- Game/Entity.cs
+++ Game/Entity.cs
@@ -1,5 +1,11 @@
+using Microsoft.Xna.Framework;
+
 namespace Client;
 
-abstract class Entity {
+abstract class Entity 
+{
+	public virtual int Health { get; set; }
 
+	public virtual void Update(GameTime gameTime) {}
+	public virtual void Draw(GameTime gameTime) {}
 }
